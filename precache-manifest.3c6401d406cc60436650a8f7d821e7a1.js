a54eccf5ba837b720f1bce317fe58fe662ce1dd4
ced <cedric.milius@yahoo.fr>
2019-05-27 00:00:22 +0200

    Updates

---
+++ precache-manifest.3c6401d406cc60436650a8f7d821e7a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "573bf9eb30ebcb98b6db",
+    "url": "/me/static/js/main.573bf9eb.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "573bf9eb30ebcb98b6db",
+    "url": "/me/static/css/main.880395b3.chunk.css"
+  },
+  {
+    "revision": "8b576e75f7d7d6de945d13151b872e40",
+    "url": "/me/index.html"
+  }
+];
