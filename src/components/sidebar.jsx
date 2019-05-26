b7f250348d6a1a16d579702a27008d796ae2a8d3
ced <cedric.milius@yahoo.fr>
2019-05-27 00:46:52 +0200

    first commit

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/about.png)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Cédric Milius</a></h1>
+              <span className="email"><i className="icon-mail"></i> cedric.milius@gmail.com</span>
+              <span className="number"><i className="icon-phone"></i> not available</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+               
+                <li><a href="https://fr.linkedin.com/in/cedricmilius" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/cultureweb" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://www.e-real.fr/" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              <p><small>
+                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
+                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration 
+              </small></p>
+              <p><small>
+                Something coming soon !!
+              </small></p>
+            </div>
+          </aside>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at AdSoftware <span>2019-present</span></h2>
+                        <p>recently have joined the AdSoftware Networs as a Software Developer-Intern. My major part of the work will be..</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Graduation at Advanced Nomades Technologies<span>2018-2019</span></h2>
+                        <p>I have taken courses like HTML5, CSS3, Wordpress, jQuery, mockup, Web-Data Management over the years and have better understanding of these subjects.I have also been graduated in Webdesign.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Background <span>2003-2015</span></h2>
+                        <p>My experience in trade, events and catering allowed me to develop adaptability and skills in communication and relationship.
+My years abroad are not an open mind and a taste for challenges.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
